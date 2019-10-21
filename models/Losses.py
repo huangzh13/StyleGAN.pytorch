b6c3eb774f4ae81f40dda0e4545349fd40c749d2
huangzh <whuhuangzh@gmail.com>
2019-10-21 17:10:41 +0800

    Add Loss function for GAN framework.

---
+++ models/Losses.py
@@ -0,0 +1,162 @@
+"""
+-------------------------------------------------
+   File Name:    Losses.py
+   Author:       Zhonghao Huang
+   Date:         2019/10/21
+   Description:  Module implementing various loss functions
+-------------------------------------------------
+"""
+
+import torch
+
+
+# =============================================================
+# Interface for the losses
+# =============================================================
+
+class GANLoss:
+    """ Base class for all losses
+
+        @args:
+        dis: Discriminator used for calculating the loss
+             Note this must be a part of the GAN framework
+    """
+
+    def __init__(self, dis):
+        self.dis = dis
+
+    def dis_loss(self, real_samps, fake_samps, height, alpha):
+        """
+        calculate the discriminator loss using the following data
+        :param real_samps: batch of real samples
+        :param fake_samps: batch of generated (fake) samples
+        :param height: current height at which training is going on
+        :param alpha: current value of the fader alpha
+        :return: loss => calculated loss Tensor
+        """
+        raise NotImplementedError("dis_loss method has not been implemented")
+
+    def gen_loss(self, real_samps, fake_samps, height, alpha):
+        """
+        calculate the generator loss
+        :param real_samps: batch of real samples
+        :param fake_samps: batch of generated (fake) samples
+        :param height: current height at which training is going on
+        :param alpha: current value of the fader alpha
+        :return: loss => calculated loss Tensor
+        """
+        raise NotImplementedError("gen_loss method has not been implemented")
+
+
+class ConditionalGANLoss:
+    """ Base class for all conditional losses """
+
+    def __init__(self, dis):
+        self.dis = dis
+
+    def dis_loss(self, real_samps, fake_samps, labels, height, alpha):
+        raise NotImplementedError("dis_loss method has not been implemented")
+
+    def gen_loss(self, real_samps, fake_samps, labels, height, alpha):
+        raise NotImplementedError("gen_loss method has not been implemented")
+
+
+# =============================================================
+# Normal versions of the Losses:
+# =============================================================
+
+class StandardGAN(GANLoss):
+
+    def __init__(self, dis):
+        from torch.nn import BCEWithLogitsLoss
+
+        super().__init__(dis)
+
+        # define the criterion and activation used for object
+        self.criterion = BCEWithLogitsLoss()
+
+    def dis_loss(self, real_samps, fake_samps, height, alpha):
+        # small assertion:
+        assert real_samps.device == fake_samps.device, \
+            "Real and Fake samples are not on the same device"
+
+        # device for computations:
+        device = fake_samps.device
+
+        # predictions for real images and fake images separately :
+        r_preds = self.dis(real_samps, height, alpha)
+        f_preds = self.dis(fake_samps, height, alpha)
+
+        # calculate the real loss:
+        real_loss = self.criterion(
+            torch.squeeze(r_preds),
+            torch.ones(real_samps.shape[0]).to(device))
+
+        # calculate the fake loss:
+        fake_loss = self.criterion(
+            torch.squeeze(f_preds),
+            torch.zeros(fake_samps.shape[0]).to(device))
+
+        # return final losses
+        return (real_loss + fake_loss) / 2
+
+    def gen_loss(self, _, fake_samps, height, alpha):
+        preds, _, _ = self.dis(fake_samps, height, alpha)
+        return self.criterion(torch.squeeze(preds),
+                              torch.ones(fake_samps.shape[0]).to(fake_samps.device))
+
+
+class HingeGAN(GANLoss):
+
+    def __init__(self, dis):
+        super().__init__(dis)
+
+    def dis_loss(self, real_samps, fake_samps, height, alpha):
+        r_preds = self.dis(real_samps, height, alpha)
+        f_preds = self.dis(fake_samps, height, alpha)
+
+        loss = (torch.mean(torch.nn.ReLU()(1 - r_preds)) +
+                torch.mean(torch.nn.ReLU()(1 + f_preds)))
+
+        return loss
+
+    def gen_loss(self, _, fake_samps, height, alpha):
+        return -torch.mean(self.dis(fake_samps, height, alpha))
+
+
+class RelativisticAverageHingeGAN(GANLoss):
+
+    def __init__(self, dis):
+        super().__init__(dis)
+
+    def dis_loss(self, real_samps, fake_samps, height, alpha):
+        # Obtain predictions
+        r_preds = self.dis(real_samps, height, alpha)
+        f_preds = self.dis(fake_samps, height, alpha)
+
+        # difference between real and fake:
+        r_f_diff = r_preds - torch.mean(f_preds)
+
+        # difference between fake and real samples
+        f_r_diff = f_preds - torch.mean(r_preds)
+
+        # return the loss
+        loss = (torch.mean(torch.nn.ReLU()(1 - r_f_diff))
+                + torch.mean(torch.nn.ReLU()(1 + f_r_diff)))
+
+        return loss
+
+    def gen_loss(self, real_samps, fake_samps, height, alpha):
+        # Obtain predictions
+        r_preds = self.dis(real_samps, height, alpha)
+        f_preds = self.dis(fake_samps, height, alpha)
+
+        # difference between real and fake:
+        r_f_diff = r_preds - torch.mean(f_preds)
+
+        # difference between fake and real samples
+        f_r_diff = f_preds - torch.mean(r_preds)
+
+        # return the loss
+        return (torch.mean(torch.nn.ReLU()(1 + r_f_diff))
+                + torch.mean(torch.nn.ReLU()(1 - f_r_diff)))
